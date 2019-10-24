bf2f0c8b4c6f24fb01b7336d527af1ebd771f8d9
Aaron Helton <aargonian@gmail.com>
2019-10-23 21:37:47 -0400

    Minor Updates to Test Script

--- test/Test.cmd
+++ test/Test.cmd
@@ -1,4 +1,8 @@
 echo off
-NameSort.exe "Sort Me.txt" > Output.txt
-fc Output.txt Sorted.txt
+
+SET OUTPUT="output.txt"
+
+if exist %OUTPUT% del /f %output%
+NameSort.exe "Sort Me.txt" > %OUTPUT%
+fc %OUTPUT% Sorted.txt
 if %errorlevel% EQU 0 (echo "Success") else ( echo "Failure")
