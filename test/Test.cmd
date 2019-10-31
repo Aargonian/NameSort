c28d9a61a5f9b475875ec6cc36f55700dd8a1bf6
Aaron Helton <aargonian@gmail.com>
2019-10-30 23:54:04 -0400

    Fixed Test.cmd to add additional tests

--- test/Test.cmd
+++ test/Test.cmd
@@ -4,6 +4,8 @@ SET OUTPUT="output.txt"
 SET OUTPUT2="output2.txt"
 SET OUTPUT3="output3.txt"
 SET OUTPUT4="output4.txt"
+SET OUTPUT5="output5.txt"
+SET OUTPUT6="output6.txt"
 
 if exist %OUTPUT% del /f %OUTPUT%
 NameSort.exe "Sort Me.txt" > %OUTPUT%
@@ -16,11 +18,21 @@ fc %OUTPUT2% "Sorted 2.txt"
 if %errorlevel% EQU 0 (echo "Success") else ( echo "Failure")
 
 if exist %OUTPUT3% del /f %OUTPUT3%
-NameSort.exe "Sort Me.txt" > %OUTPUT3%
+NameSort.exe -d "Sort Me.txt" > %OUTPUT3%
 fc %OUTPUT3% "Sorted 3.txt"
 if %errorlevel% EQU 0 (echo "Success") else ( echo "Failure")
 
 if exist %OUTPUT4% del /f %OUTPUT4%
-NameSort.exe "Sort Me 2.txt" > %OUTPUT4%
+NameSort.exe -d "Sort Me 2.txt" > %OUTPUT4%
 fc %OUTPUT4% "Sorted 4.txt"
+if %errorlevel% EQU 0 (echo "Success") else ( echo "Failure")
+
+if exist %OUTPUT5% del /f %OUTPUT5%
+NameSort.exe --descending "Sort Me.txt" > %OUTPUT5%
+fc %OUTPUT5% "Sorted 3.txt"
+if %errorlevel% EQU 0 (echo "Success") else ( echo "Failure")
+
+if exist %OUTPUT6% del /f %OUTPUT6%
+NameSort.exe --descending "Sort Me 2.txt" > %OUTPUT6%
+fc %OUTPUT6% "Sorted 4.txt"
 if %errorlevel% EQU 0 (echo "Success") else ( echo "Failure")
